2f428043bcff35c94733ef6a8cf03d7dd370fd1f
blurdel <5648737+blurdel@users.noreply.github.com>
2025-02-22 10:04:14 -0700

    Added boost directory iterator

--- boosttest.cpp
+++ boosttest.cpp
@@ -2,8 +2,24 @@
 #include <boost/array.hpp>
 
 using namespace std;
-int main() {
-  boost::array<int, 4> arr = {{1,2,3,4}};
-  cout << "hi" << arr[0];
-  return 0;
+int main()
+{
+    boost::array<int, 4> arr = {{1,2,3,4}};
+    cout << "hi" << arr[0];
+    
+    printDirs("~/Temp");
+    
+    return 0;
+}
+
+void printDirs(std::string pDir)
+{
+    path p(pDir);
+
+    if (is_directory(p)) {
+        std::cout << p << " is a directory containing:\n";
+
+        for(auto& entry : boost::make_iterator_range(directory_iterator(p), {}))
+            std::cout << entry << "\n";
+    }
 }
